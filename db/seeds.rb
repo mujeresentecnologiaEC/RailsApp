a6e6c41f4d9cdcf1be55216d809a3feaac9e7eef
Rebecca Sliter <sliterrm@gmail.com>
2014-05-28 17:46:26 -0500

    oops

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@ sectors = Sector.create([ { name: 'Norte' }, { name: 'Sur' }, { name: 'Valle' } 
 
 # Standard hobbies
 hobbies = Hobby.create([{ name: 'Leer'}, { name: 'Correr'}, { name: 'Bailar'}, { name: 'Pintar'}, { name: 'Cantar'}, { name: 'Mirar películas'} ])
+new_hobby = Hobby.create(name: "bailar SALSA")
 
 # Asistentes!
 Persona.create(name: "Rebecca", hobby: hobbies[0], sector: sectors[0])
@@ -15,7 +16,7 @@ Persona.create(name: "Helene", hobby: hobbies[3], sector: sectors[2])
 Persona.create(name: "Andrea", hobby: hobbies[5], sector: sectors[2])
 Persona.create(name: "Cris", hobby: hobbies[4], sector: sectors[2])
 Persona.create(name: "Adis", hobby: hobbies[2], sector: sectors[0])
-Persona.create(name: "Panchita", hobby: "bailar SALSA", sector: sectors[0])
+Persona.create(name: "Panchita", hobby: new_hobby, sector: sectors[0])
 Persona.create(name: "Pamela", hobby: hobbies[4], sector: sectors[0])
 Persona.create(name: "Diana", hobby: hobbies[4], sector: sectors[0])
 Persona.create(name: "Viviana", hobby: hobbies[1], sector: sectors[0])
